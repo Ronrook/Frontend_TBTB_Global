a62e4a9cc84126b3d36417878a5bc78019fd7435
Ronald Rosero <ronaldes93@gmail.com>
2024-04-25 17:03:35 -0500

    UserService is implemented

--- src/app/components/users/user.component.ts
+++ src/app/components/users/user.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  standalone: true,
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
+    standalone: true,
 })
 export class UserComponent{
   
