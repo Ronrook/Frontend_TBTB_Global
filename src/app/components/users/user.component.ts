4b36c8d4b52778b78737d1da61c576c348513da2
Ronald Rosero <ronaldes93@gmail.com>
2024-04-25 17:14:27 -0500

    UserInterface is added and UserComponent is implemented

--- src/app/components/users/user.component.ts
+++ src/app/components/users/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInterface } from '../../interfaces/user.interface';
+import { UserService } from '../../services/user.service';
 
 
 @Component({
@@ -7,7 +9,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./user.component.css'],
     standalone: true,
 })
-export class UserComponent{
-  
-  
+export class UserComponent implements OnInit {
+
+    users: UserInterface [] = [];
+    
+    
+    constructor(private userService: UserService) { }
+    
+    
+    ngOnInit(): void {
+    this.getUsers()
+    };
+    getUsers() {
+        this.userService.getUsers().subscribe({
+        next: (result) => {
+            this.users = result
+        },
+        error: (error) => {
+            console.log(error);
+        }
+        })
+    }
 }
---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+export interface UserInterface {
+
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    phone: string
+    
+}
