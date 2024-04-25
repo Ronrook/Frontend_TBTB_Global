f16a72846517ef55b29b23678ac89a12bcc0c9ff
Ronald Rosero <ronaldes93@gmail.com>
2024-04-25 17:18:28 -0500

    UserComponent es funcional

--- src/app/components/users/user.component.ts
+++ src/app/components/users/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserInterface } from '../../interfaces/user.interface';
 import { UserService } from '../../services/user.service';
+import { NgFor } from '@angular/common';
 
 
 @Component({
@@ -8,6 +9,7 @@ import { UserService } from '../../services/user.service';
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css'],
     standalone: true,
+    imports: [NgFor]
 })
 export class UserComponent implements OnInit {
 
